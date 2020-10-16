4bdcc0b26bfff84d9e20f6850ab44503daa714b5
ahtrahdis7 <sidharthabiki@gmail.com>
2020-10-16 09:52:52 +0530

    added DB fields

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,28 @@
 // import { getTotalGoals } from './Test/test'
 const express = require('express')
-const getTotalGoals = require('./Test/test')
 // Create the express app
 const app = express()
+const mongoose = require('mongoose')
+
+
+// IMPORT LOCAL PACKAGES
+const config = require('./config');
+
+// IMPORT ROUTERS
+
+
+// INIT MONGO DB
+const mongodburl = config.MONGODB_URL;
+mongoose.connect(mongodburl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+.then(console.log(`Connected to MongoDB`))
+.catch(error => {
+  console.log(error);
+});
+
 
 // Routes and middleware
 // app.use(/* ... */)
@@ -17,8 +37,10 @@ app.use(function fiveHundredHandler (err, req, res, next) {
   res.status(500).send()
 })
 
+
+
 // Start server
-app.listen(1234, function (err) {
+app.listen(config.PORT, function (err) {
   if (err) {
     return console.error(err)
   }
