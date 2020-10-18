23efbdbaaba4bc3641f9451cce3e6fa7794931d1
ahtrahdis7 <sidharthabiki@gmail.com>
2020-10-18 18:39:26 +0530

    added feature router

---
+++ backend/Routes/Features/Notice.js
@@ -0,0 +1,44 @@
+
+
+const authenticate = require('../../authenticate');
+const NoticeBoard = require('../../Models/NoticeBoard')
+const Mail = require('../../Utils/Mail')
+
+const get = (req, res, next) => {
+    NoticeBoard.find({})
+    .then((docs) => {
+        res.statusCode = 200;
+        res.json({
+            discussions: docs
+        })
+    })
+}
+
+const post = (req, res, next) => {
+    req.body.byUser = req.user._id;
+    NoticeBoard.create(req.body)
+    .then((doc) => {
+        res.statusCode = 200;
+        res.json({
+            thread: doc
+        });
+    })
+}
+
+const put = (req, res, next) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported yet');
+}
+
+const del = (req, res, next) => {
+    res.statusCode = 403;
+    res.end('DELETE operation not supported yet');
+}
+
+
+module.exports = {
+    get : get,
+    post : post,
+    put : put,
+    delete : del,
+}
